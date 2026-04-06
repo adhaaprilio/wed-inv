e460389b89662328b5847af9541d8f6c7e762ac7
adhaaprilio <adha.aprils@gmail.com>
2026-04-06 14:33:04 +0700

    test koneksi db baru

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+// Pastikan variabel env ada agar tidak crash saat build
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Supabase env variables are missing!")
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
